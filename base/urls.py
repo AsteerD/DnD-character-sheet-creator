c4f2a36cfecb1bcbd2a64102389f126c5bd7b12e
AsteerD <Asteerds@gmail.com>
2025-12-07 13:15:50 +0100

    add character view

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path # pyright: ignore[reportMissingModuleSource]
-from .views import CharacterList
+from .views import CharacterList, CharacterDetail
 
 urlpatterns = [
-    path('', CharacterList.as_view(), name='home'),
+    path('', CharacterList.as_view(), name='characters'),
+    path('character/<int:pk>/', CharacterDetail.as_view(), name='character'),
 ]

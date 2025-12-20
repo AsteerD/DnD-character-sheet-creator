41853479461cd844b8bcd040bb7e7352ee58e0d1
Your Name <you@example.com>
2025-12-20 01:07:58 +0100

    Add user accoutns, login, registration and search filters

--- base/views.py
+++ base/views.py
@@ -1,38 +1,76 @@
-from django.shortcuts import render # type: ignore
 from django.views.generic.list import ListView # type: ignore
 from django.views.generic.detail import DetailView # type: ignore
-from django.views.generic.edit import CreateView, UpdateView, DeleteView # type: ignore
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView # type: ignore
 from django.urls import reverse_lazy # type: ignore
+from django.shortcuts import render, redirect # type: ignore
+from django.contrib.auth.views import LoginView 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 from .models import Character
 
-class CharacterList(ListView):
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('characters')
+    
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('characters')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs): 
+        if self.request.user.is_authenticated:
+            return redirect('characters')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+
+class CharacterList(LoginRequiredMixin, ListView):
     model = Character
     context_object_name = 'characters'
 
-class CharacterDetail(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['characters'] = context['characters'].filter(user=self.request.user)
+        search_input = self.request.GET.get('search') or ''
+        if search_input:
+            context['characters'] = context['characters'].filter(character_name__istartswith=search_input)
+        context['search_input'] = search_input
+        return context
+
+class CharacterDetail(LoginRequiredMixin, DetailView):
     model = Character
     context_object_name = 'character'
     template_name = 'base/character.html'
 
-class CharacterCreate(CreateView):
+class CharacterCreate(LoginRequiredMixin,CreateView):
+    model = Character
+    fields = ['character_name', 'character_class', 'subclass', 'race', 'level', 'background', 'alligment', 'experience_points', 'strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma', 'armor_class', 'initiative', 'speed', 'hit_points', 'temporary_hit_points', 'hit_dice', 'death_saves_success', 'death_saves_failure', 'backstory', 'inspiration', 'languages']
+    template_name = 'base/character_form.html'
+    success_url = reverse_lazy('characters')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(CharacterCreate, self).form_valid(form)
+
+class CharacterUpdate(LoginRequiredMixin, UpdateView):
     model = Character
     fields = '__all__'
     template_name = 'base/character_form.html'
     success_url = reverse_lazy('characters')
 
-class CharacterUpdate(UpdateView):
+class CharacterDelete(LoginRequiredMixin, DeleteView):
     model = Character
-    fields = '__all__'
-    template_name = 'base/character_form.html'
-    success_url = reverse_lazy('characters')
-
-class CharacterDelete(DeleteView):
-    model = Character
-    template_name = 'base/character_confirm_delete.html'
-    success_url = reverse_lazy('characters')
-
-class CharacterDelete(DeleteView):
-    model = Character
-    context_object_name = 'character'
     template_name = 'base/character_confirm_delete.html'
     success_url = reverse_lazy('characters')

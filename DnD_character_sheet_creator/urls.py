531df0de5bdb32c582b1ae67b6326d2f72cf6edb
rabarbarmabarbar <martyna.barc.pati@gmail.com>
2026-01-23 15:06:52 +0100

    Update urls.py

--- DnD_character_sheet_creator/urls.py
+++ DnD_character_sheet_creator/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin # type: ignore
-from django.urls import path, include # type: ignore
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('base.urls')),
+
+    # HTML
+    path('', include('base.urls')),
+
+    # API
+    path('api/', include('base.api_urls')),
 ]
+
